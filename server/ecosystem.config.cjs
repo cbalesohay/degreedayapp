55f694d946c9923fcb9409db52e230f326e21255
cbalesohay <kawipro32@gmail.com>
2025-01-07 13:15:37 -0800

    Adjusting server.js

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -1,34 +1,7 @@
-// module.exports = {
-//   apps : [{
-//     script: 'index.js',
-//     watch: '.'
-//   }, {
-//     script: './service-worker/',
-//     watch: ['./service-worker']
-//   }],
-
-//   deploy : {
-//     production : {
-//       user : 'SSH_USERNAME',
-//       host : 'SSH_HOSTMACHINE',
-//       ref  : 'origin/master',
-//       repo : 'GIT_REPOSITORY',
-//       path : 'DESTINATION_PATH',
-//       'pre-deploy-local': '',
-//       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-//       'pre-setup': '',
-
-//     }
-//   }
-// };
-
-
-
 module.exports = {
   apps: [
     {
-      name: "DegreeDay",
-      script: "./server.js",
+      script: "server.js",
       env: {
         NODE_ENV: "development",
       },
@@ -36,6 +9,23 @@ module.exports = {
         NODE_ENV: "production",
         PORT: process.env.PORT,
       },
+      watch: ".",
     },
   ],
 };
+
+// module.exports = {
+//   apps: [
+//     {
+//       name: "DegreeDay",
+//       script: "./server.js",
+//       env: {
+//         NODE_ENV: "development",
+//       },
+//       env_production: {
+//         NODE_ENV: "production",
+//         PORT: process.env.PORT,
+//       },
+//     },
+//   ],
+// };
